c4fc6df2da003ad95964a90258f7db4ce7802afd
Ronit Mankad <mankadronit@users.noreply.github.com>
2024-03-09 16:24:15 -0500

    made changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -186,7 +186,7 @@ const techStack = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  showGithubProfile: true, // Set true or false to show Contact profile using Github, defaults to true
   display: true // Set false to hide this section, defaults to true
 };
 
