da1e77e7c25773568e42f8631b9ea3e721872d9d
Ronit Mankad <mankadronit@users.noreply.github.com>
2024-03-28 17:28:04 -0400

    add

---
+++ src/Profile.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect, lazy, Suspense} from "react";
+import {openSource} from "../../portfolio";
+import Contact from "../contact/Contact";
+import Loading from "../loading/Loading";
+import profile from "../../profile.json";
+
+const renderLoader = () => <Loading />;
+const GithubProfileCard = lazy(() =>
+  import("../../components/githubProfileCard/GithubProfileCard")
+);
+export default function Profile() {
+  const [prof, setrepo] = useState([]);
+  function setProfileFunction(array) {
+    setrepo(array);
+  }
+
+  useEffect(() => {
+    if (openSource.showGithubProfile === "true") {
+      /* Hack since my own domain space not working to pull data from public
+      const getProfileData = () => {
+        fetch("/profile.json")
+          .then(result => {
+            if (result.ok) {
+              return result.json();
+            }
+          })
+          .then(response => {
+            setProfileFunction(response.data.user);
+          })
+          .catch(function (error) {
+            console.error(
+              `${error} (because of this error GitHub contact section could not be displayed. Contact section has reverted to default)`
+            );
+            setProfileFunction("Error");
+            openSource.showGithubProfile = "false";
+          });
+      };
+      getProfileData();
+      */
+      setProfileFunction(profile.data.user);
+    }
+  }, []);
+  if (
+    openSource.display &&
+    openSource.showGithubProfile === "true" &&
+    !(typeof prof === "string" || prof instanceof String)
+  ) {
+    return (
+      <Suspense fallback={renderLoader()}>
+        <GithubProfileCard prof={prof} key={prof.id} />
+      </Suspense>
+    );
+  } else {
+    return <Contact />;
+  }
+}
